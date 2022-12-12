93d0a194c86760707287f77344174f2ab1a6fec2
Logan <logancolestevens@gmail.com>
2022-12-12 14:19:35 -0500

    10 - Controller Support

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -45,6 +45,12 @@ public class CarController : MonoBehaviour
     }
 
     private void FixedUpdate() {
+
+        // Camera Rig Movement Control
+    
+        ControllerCheck();
+
+    //--------------------------------------------
         
         finish = Block.getBlockByID(gridScript.list, 14);
         if (numOfFrames < 30) {
@@ -58,7 +64,7 @@ public class CarController : MonoBehaviour
         HandleSteering();
         UpdateWheels();
 
-        if (Input.GetKey(KeyCode.Q)) {
+        if (Input.GetKey(KeyCode.Q) || Input.GetButton("XboxX") || Input.GetButton("XboxRB")) {
             transform.position = new Vector3(-((finish.x * 15) + 8), 0.25f , (finish.y * 15) + 8);
             transform.eulerAngles = new Vector3(0.0f, finish.angle, 0.0f);
             ApplyBreaking();
@@ -102,11 +108,10 @@ public class CarController : MonoBehaviour
     }
 
 
-    private void GetInput()
-    {
+    private void GetInput() {
         horizontalInput = Input.GetAxis(HORIZONTAL);
         verticalInput = Input.GetAxis(VERTICAL);
-        isBreaking = Input.GetKey(KeyCode.Space);
+        isBreaking = Input.GetKey(KeyCode.Space) || Input.GetButton("XboxA");
     }
 
     private void HandleMotor()
@@ -155,5 +160,40 @@ public class CarController : MonoBehaviour
 
     int abs(int a) {
         return a < 0 ? -a : a;
+    }
+    void ControllerCheck() {
+        // float ltaxis = Input.GetAxis("XboxLeftTrigger");
+        // float rtaxis = Input.GetAxis("XboxRightTrigger");
+        // float dhaxis = Input.GetAxis("XboxDpadHorizontal");
+        // float dvaxis = Input.GetAxis("XboxDpadVertical");
+    
+        bool xbox_a = Input.GetButton("XboxA");
+        //bool xbox_b = Input.GetButton("XboxB");
+        //bool xbox_x = Input.GetButton("XboxY");
+        bool xbox_y = Input.GetButton("XboxX");
+        // bool xbox_lb = Input.GetButton("XboxLB");
+        // bool xbox_rb = Input.GetButton("XboxRB");
+        // bool xbox_ls = Input.GetButton("XboxLS");
+        // bool xbox_rs = Input.GetButton("XboxRS");
+        // bool xbox_view = Input.GetButton("XboxView");
+        // bool xbox_menu = Input.GetButton("XboxMenu");        
+    
+        string DebugText = (""+
+            // "Horizontal: {14:0.000} Vertical: {15:0.000}\n" +
+            // "HorizontalTurn: {16:0.000} VerticalTurn: {17:0.000}\n" +
+            // "LTrigger: {0:0.000} RTrigger: {1:0.000}\n" +
+            // "A: {2} B: {3} X: {4} Y:{5}\n" +
+            // "LB: {6} RB: {7} LS: {8} RS:{9}\n" +
+            // "View: {10} Menu: {11}\n" +
+            // "Dpad-H: {12:0.000} Dpad-V: {13:0.000}\n" +
+            //ltaxis + rtaxis +
+            xbox_a + /* xbox_b + xbox_x +  */ xbox_y
+            // +
+            // xbox_lb+ xbox_rb+ xbox_ls+ xbox_rs+
+            // xbox_view+ xbox_menu
+            //dhaxis+dvaxis
+            );
+        
+        Debug.Log(DebugText);
     }
 }
